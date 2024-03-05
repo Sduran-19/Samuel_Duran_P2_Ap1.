187508aea369f6b6e5c4f6f44389b751aae2c021
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-03-04 20:54:20 -0400

    agregando contexto

--- Samuel_duran_P2_Ap1.Api/DAL/Contexto.cs
+++ Samuel_duran_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Samuel_Duran_P2_Ap1.Models;
 
 namespace Samuel_duran_P2_Ap1.DAL
 {
     public class Contexto : DbContext
     {
-      
-       public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Vehiculo>? Vehiculo { get; set; }
+
+        public DbSet<VehiculoDetalle>? VehiculoDetalle { get; set; }
+
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     }
 }
