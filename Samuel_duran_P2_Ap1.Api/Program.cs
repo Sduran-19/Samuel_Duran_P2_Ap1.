c2a0289221fa339efceba22fd548e151b0b58ac8
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-03-04 21:21:41 -0400

    ultimo commit

--- Samuel_duran_P2_Ap1.Api/Program.cs
+++ Samuel_duran_P2_Ap1.Api/Program.cs
@@ -22,6 +22,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
